d802c51afa08948bf09e016c25f3f56101ec6abd
pabloandura <85837774+pabloandura@users.noreply.github.com>
2021-11-27 15:12:52 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,51 +1,43 @@
+//lado de cuadrado bidimensional
+#define LED 8
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-//archivo de prototipos
-#include "proto.h"
-//archivo de funciones
-#include "func.h"
-// Pablo Javier Lopez Andura - 109947 DNI 38-456-394 UTN AVELLANEDA - INFORMATICA II - DAMIAN CORBALAN Y GUSTAVO VIARD
+//incluimos la libreria de tiempos time.h
+#include <time.h>
+//libreria de typedef definidos por autor
+#include "tipos.h"
+//libreria funciones
+#include "funciones.h"
 
 
-//macro setbit
-#define setBit(reg,bit) reg|(1<<bit)
-#define tamanoPalabra 4
+//prototipos
+void ritmo(int *bpm);
+void fijado(int *n);
+void prender(int, struct memoria *mem);
 
-int i=0;
-int operador=1;
+int main(int comandoInt, char *comandoVect)
+{
+    /**
+    "b_bpm" es la funcion local al main que
+    almacena en dinamica el valor de bpm
+    para la funcion ritmo
 
+    "e" es la variable para cambio de estado.
 
-
-int main(int argc, char const *argv[]){
-
-//inicializo las strings
-
-    dat_t p;
-
-    p.aniNro=0;
-    char auxiliar[1]="\0";
-    /*
-    while(1)
-    {
-        if(operador==1){
-            // archivo de memorias de animacion personalizadas
-        }
-    }
+    "op" dicta el fin del programa
     */
-    strcpy(p.palabra1,"0001");
-    strcpy(p.palabra2,"0010");
-    strcpy(p.palabra3,"0100");
-    strcpy(p.palabra4,"1000");
 
-    //comienzo de maquina de estados
-
-
-        ascendente(p);
-
-        printf("Operador( 0 sale ): \n");
-        scanf("%d", &operador);
-
-    p.aniNro=0;
+    int e=0,b_bpm=120,op=0,n=100;
+    //pregunta de estado
+    do{
+        if(e)
+            ritmo(b_bpm);
+        else
+            fijado(n);
+        // se escucha musica?
+        e=detectar();
+       }while(op);
+//    printf("Bienvenid@\n");
     return 0;
 }
